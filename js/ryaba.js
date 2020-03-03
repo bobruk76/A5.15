6a49396e89dad2bf6a6b48b5f6a63ceb9fec14b0
Vladimir Poliakov <vladimir.m.polyakov@gmail.com>
2020-03-03 20:42:35 +0300

    form ready

--- js/ryaba.js
+++ js/ryaba.js
@@ -1,8 +1,8 @@
 const dataURL = "https://api.myjson.com/bins/jcmhn";
+const $form = document.getElementById('messages');
 const $result = $("#result");
 const $fetchButton = $("#button-fetch")
 var fields = [];
-
 
 function handleButton() {
 
@@ -13,14 +13,28 @@ $fetchButton.click(handleButton);
 function handleData(data) {
 
 }
-/*	$("#button-fetch").click(handleButton);*/
+
  function init() {
+      const template = info =>{
+        const $field = $(`<div class="form-group">
+        <label for="${info}">${info}</label>
+        <input type="text" class="form-control" name="${info}" placeholder="${info}" value="">
+        </div>`)
+        $field.appendTo($form);
+      }
+
+
     $.getJSON(dataURL, function(data) {
-    for(var i in data.text) {
+    for(let i in data.text) {
       $result.append('<span>'+data.text[i]+'</span>');
       Array.prototype.push.apply(fields,data.text[i].match(/{[a-z][a-z0-9]*}/gi));
     }
+    fields = Array.from(new Set(fields));
     console.log(fields);
+
+    for(let i in fields) {
+      template(fields[i].replace('{','').replace('}',''))
+    }
   }
 )};
 

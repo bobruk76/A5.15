a701537d5ba05d38c19b1bb5b20420cd2fcfd375
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-03-04 16:04:35 +0300

    end task

--- js/ryaba.js
+++ js/ryaba.js
@@ -7,13 +7,14 @@ var fields = [];
 
 
 function handleButton() {
-  let $content = $("#result>p");
-  let request=$form.serializeArray();
-
+  const request=$form.serializeArray();
   for(let i in request){
-    $content.html(function(j,oldText){
+
+    if(request[i].value.length>0) {
+      $('#'+request[i].name).attr('readonly','readonly');
+      $result.html(function(j,oldText){
       return oldText.split(`{${request[i].name}}`).join(`<span class="badge badge-success">${request[i].value}</span>`)
-    })
+    })}
   }
 
   return false;
@@ -26,7 +27,7 @@ function init() {
         const $field = $(`<div class="form-group row">
         <label class="col-sm-2 col-form-label" for="${info}">${info}</label>
         <div class="col-sm-10">
-        <input type="text" class="form-control" name="${info}" placeholder="${info}" value="${info}">
+        <input type="text" class="form-control" id="${info}" name="${info}" placeholder="${info}" value="">
         </div></div>`)
         $field.insertBefore($bt__wrap);
       }
@@ -37,7 +38,6 @@ function init() {
       Array.prototype.push.apply(fields,data.text[i].match(/{[a-z][a-z0-9]*}/gi));
     }
     fields = Array.from(new Set(fields));
-    console.log(fields);
 
     for(let i in fields) {
       template(fields[i].replace('{','').replace('}',''))

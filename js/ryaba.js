3614022345c3951fed8f33f0b9f20ca43608d003
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-03-05 10:05:15 +0300

    redefine any function

--- js/ryaba.js
+++ js/ryaba.js
@@ -3,45 +3,42 @@ const $form = $('#messages');
 const $bt__wrap = $('#messages>.bt-wrap');
 const $fetchButton = $("#button-fetch")
 const $result = $("#result>p");
-var fields = [];
 
 
-function handleButton() {
+
+function handleButton(event) {
   const request=$form.serializeArray();
-  for(let i in request){
-
-    if(request[i].value.length>0) {
-      $('#'+request[i].name).attr('readonly','readonly');
+  request.forEach(Item => {
+    if(Item.value.length>0) {
+      $('#'+Item.name).attr('readonly','readonly');
       $result.html(function(j,oldText){
-      return oldText.split(`{${request[i].name}}`).join(`<span class="badge badge-success">${request[i].value}</span>`)
+      return oldText.split(`{${Item.name}}`).join(`<span class="badge badge-success">${Item.value}</span>`)
     })}
-  }
-
-  return false;
+  })
+  event.preventDefault();
 }
 
-$fetchButton.click(handleButton)
-
 function init() {
-      const template = info =>{
-        const $field = $(`<div class="form-group row">
-        <label class="col-sm-2 col-form-label" for="${info}">${info}</label>
-        <div class="col-sm-10">
-        <input type="text" class="form-control" id="${info}" name="${info}" placeholder="${info}" value="">
-        </div></div>`)
-        $field.insertBefore($bt__wrap);
-      }
-
+  $fetchButton.click(handleButton)
+  const template = info =>{
+    const $field = $(`<div class="form-group row">
+    <label class="col-sm-2 col-form-label" for="${info}">${info}</label>
+    <div class="col-sm-10">
+    <input type="text" class="form-control" id="${info}" name="${info}" placeholder="${info}" value="">
+    </div></div>`)
+    $field.insertBefore($bt__wrap);
+  }
+    let fields = [];
     $.getJSON(dataURL, function(data) {
-    for(let i in data.text) {
-      $result.append(data.text[i]);
-      Array.prototype.push.apply(fields,data.text[i].match(/{[a-z][a-z0-9]*}/gi));
-    }
+      data.text.forEach(Item => {
+      $result.append(Item);
+      Array.prototype.push.apply(fields,Item.match(/{[a-z][a-z0-9]*}/gi));
+    })
     fields = Array.from(new Set(fields));
 
-    for(let i in fields) {
-      template(fields[i].replace('{','').replace('}',''))
-    }
+    fields.forEach(Item => {
+      template(Item.replace('{','').replace('}',''))
+    })
   }
 )};
 
